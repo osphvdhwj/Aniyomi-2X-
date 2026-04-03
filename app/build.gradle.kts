62b3577179d1a3d375aeabf9c82de6d05916c35e
osphvdhwj <150425690+osphvdhwj@users.noreply.github.com>
2026-04-03 16:16:01 +0530

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.dark.animetailv2.module"
+    compileSdk = 34
+    defaultConfig {
+        applicationId = "com.dark.animetailv2.module"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+    buildFeatures { viewBinding = true }
+}
+
+dependencies {
+    compileOnly("de.robv.android.xposed:api:101")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.10.0")
+}
