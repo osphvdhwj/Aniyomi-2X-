b7419a2a93f9193da8b135aed661cbfe6b21665d
osphvdhwj <manjukhatri0101@gmail.com>
2026-05-31 19:26:19 +0530

    v2.1 Fix: Added missing Gravity import and refined UI code

--- app/src/main/java/com/dark/animetailv2/module/SettingsActivity.kt
+++ app/src/main/java/com/dark/animetailv2/module/SettingsActivity.kt
@@ -1,12 +1,14 @@
 package com.dark.animetailv2.module
 
 import android.app.Activity
+import android.app.AlertDialog
 import android.content.Context
 import android.content.res.ColorStateList
 import android.graphics.Color
 import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import android.text.InputType
+import android.view.Gravity
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
